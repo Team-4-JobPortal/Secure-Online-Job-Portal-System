93f2d7a6074d85b8fe2cfa9b0efd08ea8aeab42c
yash <ykushawa@montran.com>
2025-08-21 10:54:01 +0530

    add the missing file

---
+++ src/main/java/com/example/exception/UserEmailExistException.java
@@ -0,0 +1,10 @@
+package com.example.exception;
+
+public class UserEmailExistException  extends RuntimeException{
+	public UserEmailExistException(String message) {
+		super(message);
+	}
+	
+	
+
+}
